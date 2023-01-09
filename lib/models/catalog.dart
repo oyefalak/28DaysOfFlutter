3edd9d096bebad56ecb2e4c5a3eeadc37d6db7b0
oyefalak <sabbirrajput03@gmail.com>
2023-01-09 15:32:21 +0530

    Adding Items manually in a catalog

---
+++ lib/models/catalog.dart
@@ -0,0 +1,22 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+
+}
+final products = [
+  Item(
+      id: "Codepur001",
+      name: "iPhone 12 Pro",
+      desc: "Apple iPhone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+];
+
