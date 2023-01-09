1aa02cc0090d93dce2524ee9a6270c62925078a9
oyefalak <sabbirrajput03@gmail.com>
2023-01-09 11:32:24 +0530

    created login page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "By Shabbir";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days Days Of Flutter $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
