64fba720bf683c962fda9d96a082257a7602def0
oyefalak <sabbirrajput03@gmail.com>
2023-01-11 19:06:25 +0530

    Learn Mapping and Data Class Generator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,38 +1,36 @@
-// ignore_for_file: unnecessary_null_comparison, unused_element
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:my_first_app/models/catalog.dart';
-import '../widgets/drawer.dart';
-import '../widgets/item_widget.dart';
+import 'package:my_first_app/widgets/drawer.dart';
+import 'package:my_first_app/widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  //final int days = 30;
+  final int days = 30;
 
-  //final String name = "By Shabbir";
+  final String name = "Codepur";
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    loadData() async {
-      await Future.delayed(const Duration(seconds: 2));
-      final catalogJson = await rootBundle.loadString('assets/files/catalog.json');
-      final decodedData = jsonDecode(catalogJson);
-      var productsData = decodedData["products"];
-      CatalogModel.items = List.from(productsData)
-          .map<Item>((item) => Item.fromMap(item))
-          .toList();
-      setState(() { });
-    }
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+    await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
   }
 
   @override
@@ -45,11 +43,11 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
-            itemCount: CatalogModel.items.length,
-            itemBuilder: (context, index) =>
-               ItemWidget(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) =>
+              ItemWidget(
                 item: CatalogModel.items[index],
-               ),
+              ),
         ):const Center(
           child: CircularProgressIndicator(),
         ),
