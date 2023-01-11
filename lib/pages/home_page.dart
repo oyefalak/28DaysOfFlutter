53e176ae3998e39478f7631b250d35d58bc1e692
oyefalak <sabbirrajput03@gmail.com>
2023-01-11 12:17:40 +0530

    Learn How to load Json,creating files and Decoding and also how to debugging in flutter

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:my_first_app/models/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "By Shabbir";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData() async {
+      final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+      final decodedData = jsonDecode(catalogJson);
+      var productsData = decodedData["products"];
+      print(productsData);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
